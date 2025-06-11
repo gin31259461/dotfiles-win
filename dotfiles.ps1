bd068f6d71a9c4a04af87a93772f0024f5e75519
Abner <qw0207060413@gmail.com>
2025-06-11 14:06:24 +0800

    sync dotfiles

--- dotfiles.ps1
+++ dotfiles.ps1
@@ -4,7 +4,7 @@ dot add README.md dotfiles.ps1
 dot add .config/wezterm
 dot add .config/visual-studio
 dot add .vimrc
-dot add %AppData%\Roaming\alacritty
+dot add $env:APPDATA\Roaming\alacritty
 
 
 # push
