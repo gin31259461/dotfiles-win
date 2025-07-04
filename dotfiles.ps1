0f1fdd9942746ee01efadfb28b1c5bf62e72890f
Abner <qw0207060413@gmail.com>
2025-07-04 21:18:01 +0800

    sync dotfiles

--- dotfiles.ps1
+++ dotfiles.ps1
@@ -4,6 +4,7 @@ dot add README.md .gitmodules dotfiles.ps1
 dot add .config/wezterm
 dot add .config/visual-studio
 dot add .config/vscode-nvim
+dot add .config/ssms
 dot add .config/nvim
 dot add .vimrc
 dot add ./AppData/Roaming/alacritty/
